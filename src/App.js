320798c0b31e0f67deececceb9e110ad443f1297
AlexandrZeroff <zeroffalexandr@gmail.com>
2023-03-10 18:21:50 +0200

    Started appscreen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
 import AppHeader from './components/appHeader.jsx/AppHeader';
+import AppScreen from './components/appScreen/AppScreen';
 
 function App() {
   return (
-    <AppHeader />
+    <>
+      <AppHeader />
+      <AppScreen />
+    </>
   );
 }
 
