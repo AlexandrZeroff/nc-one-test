dcc175883437f321db18ea04195a98290a3731a6
AlexandrZeroff <zeroffalexandr@gmail.com>
2023-03-11 10:10:13 +0200

    API hook added

--- src/components/goodsList/GoodsList.jsx
+++ src/components/goodsList/GoodsList.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import useProducts from "../../hooks/useProducts";
 
 const GoodsList = () => {
-  return (
-    <div>GoodsList</div>
-  )
-}
+  const products = useProducts();
+  console.log(products)
 
-export default GoodsList
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        p: 4,
+        boxSizing: "border-box",
+      }}
+    >
+      {/* {products.map((product, index) => (
+        <div>{product}</div>
+      ))} */}
+    </Box>
+  );
+};
+
+export default GoodsList;
